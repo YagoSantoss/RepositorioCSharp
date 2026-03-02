69010df62d3ca6d4dbdba4dfc7f7bf69fdfbdb6a
YagoSantoss <yagomib971@gmail.com>
2026-03-02 14:16:45 -0300

    subindo atividades

---
+++ Dia da Semana/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Dia_da_Semana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPergunta = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtResposta = new System.Windows.Forms.TextBox();
+            this.lblDia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPergunta
+            // 
+            this.lblPergunta.AutoSize = true;
+            this.lblPergunta.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPergunta.Location = new System.Drawing.Point(301, 75);
+            this.lblPergunta.Name = "lblPergunta";
+            this.lblPergunta.Size = new System.Drawing.Size(208, 25);
+            this.lblPergunta.TabIndex = 0;
+            this.lblPergunta.Text = "Digite um numero";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(350, 180);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(111, 30);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnEnviar_MouseClick);
+            // 
+            // txtResposta
+            // 
+            this.txtResposta.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResposta.Location = new System.Drawing.Point(340, 125);
+            this.txtResposta.Name = "txtResposta";
+            this.txtResposta.Size = new System.Drawing.Size(130, 33);
+            this.txtResposta.TabIndex = 2;
+            this.txtResposta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtResposta_KeyPress);
+            // 
+            // lblDia
+            // 
+            this.lblDia.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDia.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDia.Location = new System.Drawing.Point(301, 223);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(208, 25);
+            this.lblDia.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDia);
+            this.Controls.Add(this.txtResposta);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.lblPergunta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPergunta;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtResposta;
+        private System.Windows.Forms.Label lblDia;
+    }
+}
+
---
+++ Verificação de Estoque/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Verificação_de_Estoque
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPergunta = new System.Windows.Forms.Label();
+            this.txtResposta = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPergunta
+            // 
+            this.lblPergunta.AutoSize = true;
+            this.lblPergunta.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPergunta.Location = new System.Drawing.Point(196, 74);
+            this.lblPergunta.Name = "lblPergunta";
+            this.lblPergunta.Size = new System.Drawing.Size(384, 25);
+            this.lblPergunta.TabIndex = 0;
+            this.lblPergunta.Text = "Quantos produtos tem no estoque?";
+            // 
+            // txtResposta
+            // 
+            this.txtResposta.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResposta.Location = new System.Drawing.Point(307, 129);
+            this.txtResposta.Name = "txtResposta";
+            this.txtResposta.Size = new System.Drawing.Size(154, 33);
+            this.txtResposta.TabIndex = 1;
+            this.txtResposta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Cursor = System.Windows.Forms.Cursors.PanNorth;
+            this.btnEnviar.Location = new System.Drawing.Point(350, 188);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnEnviar_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtResposta);
+            this.Controls.Add(this.lblPergunta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPergunta;
+        private System.Windows.Forms.TextBox txtResposta;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
